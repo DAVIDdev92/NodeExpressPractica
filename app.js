a4543372bb3b9edb6b35a108863b6291e007c920
David <david.espejo.repiso@gmail.com>
2023-12-12 17:35:24 +0100

    APIrestful routeado

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const http = require("http");
-
+//importamos el modulo de express//
+const express = require("express");
+ 
+//declaramos el puerto donde se levantará el servidor//
 const PORT = 3000;
+ 
+//asi inicializamos express y podemos acceder a todas las funcionalidades que nos proporciona//
+const app = express();
+ 
+//analizamos los archivos JSON//
+app.use(express.json());
+ 
+const users = require("./controllers/userscontroler");
+app.use("/users", users);
+ 
+app.listen(PORT, () => {
+  console.log(`Server running http://localhost:${PORT}`);
+});
 
-const server = http.createServer( (req, res) => {
-    res.setHeader("Content-type", "text/html; charset = utf-8");
-    res.end("<h1> Hola Mundo </h1>");
-})
-
-server.listen(PORT, () => {
-    console.log(`Server running at http:localhost:${PORT}`)
-})
---
+++ controllers/userscontroler.js
@@ -0,0 +1,80 @@
+let USERS = [
+  { id: 1, name: 'Usuario 1', email: 'usuario1@example.com' },
+  { id: 2, name: 'Usuario 2', email: 'usuario2@example.com' },
+  { id: 3, name: 'Usuario 3', email: 'usuario3@example.com' }
+];
+
+// para obtener datos
+const getUsers = (req, res) => {
+  res.send(USERS);
+}
+
+// para obtener datos concretos
+const getUserById = (req, res) => {
+  const userId = parseInt(req.params.id)
+  const user = USERS.find(user => user.id == userId)
+
+  res.send(user)
+}
+
+// para actualizar algo que existe usamos el patch!!!.
+const updateById = (req, res) => {
+  const userId = parseInt(req.params.id)
+
+  const user = USERS.find(user => user.id == userId)
+
+  const { name, email } = req.body
+  if (!user) {
+      res.send("No existe el usuario")
+
+  } else {
+
+      if (name) {
+          user.name = name
+      }
+
+      if (email) {
+          user.email = email
+      }
+
+      res.send(user)
+
+  }
+}
+
+// Para crear un dato nuevo
+const postUser = (req, res) => {
+  let newIndex = USERS.length + 1
+  const { name, email } = req.body
+
+  const newUser = {
+      id: newIndex,
+      name: !name ? `Usuario ${newIndex}` : name,
+      email: !email ? `usuario${newIndex}@example.com` : email
+  }
+  
+  USERS.push(newUser)
+
+  res.send(newUser)
+}
+
+// Para borrar algo.
+const delUserById = (req, res) => {
+  const userId = parseInt(req.params.id)
+  const userFilter = USERS.filter(user => user.id !== userId)
+
+  if (userFilter.length === USERS.length) {
+      res.send("No existe el usuario")
+  } else {
+      USERS = userFilter
+      res.send(USERS)
+  }
+}
+
+module.exports = {
+  getUsers, 
+  getUserById,
+  updateById,
+  postUser,
+  delUserById
+};
