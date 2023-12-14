0e4e7edb9c2acbcd0ef2d9c9a4dbdfe6328550fb
David <david.espejo.repiso@gmail.com>
2023-12-15 00:25:49 +0100

    ejercicio NODEjs

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
-//importamos el modulo de express//
+// Importamos el modulo de express
 const express = require("express");
- 
-//declaramos el puerto donde se levantará el servidor//
+// Importamos mongo
+const mongoose = require("mongoose");
+// Declaramos el puerto donde se levantará el servidor
 const PORT = 3000;
- 
-//asi inicializamos express y podemos acceder a todas las funcionalidades que nos proporciona//
+const userRouter = require("./router/userRoutes");
+
+//Así inicializamos express y podemos acceder a todas las funcionalidades que nos proporciona
 const app = express();
- 
-//analizamos los archivos JSON//
+
+//Analizamos los archivos JSON
 app.use(express.json());
- 
-const users = require("./controllers/userscontroler");
-app.use("/users", users);
- 
+
+// Esto nos permite obtener la información de configuración de ".env"
+require("dotenv").config();
+
+const url_mongo = process.env.DATABASE_URL_DEV;
+
+mongoose.connect(url_mongo);
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.log(`Error al conectar con mongo ${error}`);
+});
+
+db.on("connected", () => {
+  console.log(`Succecss connect`);
+});
+
+db.on("disconected", () => {
+  console.log(`Mongo is disconected`);
+});
+
+app.use("/users", userRouter);
+
 app.listen(PORT, () => {
   console.log(`Server running http://localhost:${PORT}`);
 });
-
